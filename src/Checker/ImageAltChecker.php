a43edd2e7fc73664cb147e04c9e4cfe37f7fe8a0
Hermann Käser <hermann.kaser@gmail.com>
2019-09-23 13:25:00 -0400

    Creates ImageAltChecker

---
+++ src/Checker/ImageAltChecker.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace SeoAnalyser\Checker;
+
+use SeoAnalyser\Sitemap\Error;
+use Symfony\Component\DomCrawler\Crawler;
+use Tightenco\Collect\Support\Collection;
+
+class ImageAltChecker implements CheckerInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function check(Crawler $crawler): Collection
+    {
+        $errors = new Collection;
+
+        /** @var \DomElement $imageNode */
+        foreach ($crawler->filterXPath('//img') as $imageNode) {
+            if (empty(trim($imageNode->getAttribute('alt')))) {
+                $errors->push(
+                    new Error(
+                        sprintf('Missing or empty <img> alt attribute for %s', $imageNode->getAttribute('src')),
+                        Error::SEVERITY_LOW
+                    )
+                );
+            }
+        }
+
+        return $errors;
+    }
+}
